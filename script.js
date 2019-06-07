6d2239b95a774342c9bfe3b8656cfa88dc0b46dd
Camilla Gejl Olsen <cami32d9@stud.kea.dk>
2019-06-07 12:42:58 +0200

    Things I haven't kept track of...

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 document.addEventListener("DOMContentLoaded", start);
 
 function start() {
-    document.querySelector("#splash_btn").addEventListener("click", function() {
-        document.querySelector("#splash").classList.add("hide_splash");
-        // let div = document.querySelector("#splash");
-        // div.scrollTop = div.scrollHeight - div.clientHeight;
-        // console.log(div);
-    })
+    console.log("Murmurfitsl")
 }
